00f84991462a0f379f640748d201e84368e70f01
Huaqiang Ye <huaqiangy16@g.ucla.edu>
2024-04-29 10:16:56 -0700

    main
    main

---
+++ rogalik-main/src/entities/player.py
@@ -0,0 +1,271 @@
+import pygame
+from math import sqrt
+from src.objects.p import Poop
+from src.objects.flask import GreenFlask
+from .entity import Entity
+from src.particles import Dust
+
+def recovery(client_angle):
+    angle = 0
+    attack_area = 0
+    if(client_angle == 1):
+        angle = 345
+        attack_area = 1
+    elif(client_angle == 2):
+        angle = 330
+        attack_area = 1
+    elif(client_angle == 3):
+        angle = 315  
+        attack_area = 1  
+    elif(client_angle == 4):
+        angle = 300
+        attack_area = 1
+    elif(client_angle == 5):
+        angle = 285  
+        attack_area = 1  
+    elif(client_angle == 6):
+        angle = 270  
+        attack_area = 1  
+    elif(client_angle == 7):
+        angle = 255
+        attack_area = 1
+    elif(client_angle == 8):
+        angle = 240 
+        attack_area = 1   
+    elif(client_angle == 9):
+        angle = 225   
+        attack_area = 1 
+    elif(client_angle == 10):
+        angle = 210
+        attack_area = 1
+    elif(client_angle == 11):
+        angle = 195 
+        attack_area = 1   
+    elif(client_angle == 12):
+        angle = 180  
+        attack_area = 1  
+    elif(client_angle == 13):
+        angle = 0
+        attack_area = 1
+    elif(client_angle == 14):
+        angle = 15 
+        attack_area = 1   
+    elif(client_angle == 15):
+        angle = 30  
+        attack_area = 1  
+    elif(client_angle == 16): 
+        angle = 45
+        attack_area = 1
+    elif(client_angle == 17):
+        angle = 60
+        attack_area = 1
+    elif(client_angle == 18):
+        angle = 75 
+        attack_area = 1
+    elif(client_angle == 19):
+        angle = 90
+        attack_area = 1
+    elif(client_angle == 20):
+        angle = 105
+        attack_area = 1
+    elif(client_angle == 21):
+        angle = 120
+        attack_area = 1
+    elif(client_angle == 22):
+        angle = 135
+        attack_area = 1
+    elif(client_angle == 23):  
+        angle = 150
+        attack_area = 1
+    elif(client_angle == 24):      
+        angle = 165
+        attack_area = 1
+    elif(client_angle == 25):
+        attack_area = 0
+    
+    return attack_area,angle
+
+class Player(Entity):
+    name = 'player'
+    speed = 360
+    max_hp = 100
+    gold = 0
+    shield = 1
+    strength = 1
+    hp = max_hp
+    items = []
+
+    def __init__(self, game):
+        Entity.__init__(self, game, self.name)
+        self.rect = self.image.get_rect(center=(512 + 2.5 * 64, 600))
+        self.weapon = None
+        self.attacking = False
+        self.interaction = True
+        self.attack_cooldown = 350  # ms
+        self.room = None
+        self.death_counter = 1
+        self.falling = False
+        self.floor_value = self.rect.y
+        self.fall(-100)
+        self.keydeterm = {"K_w":False, "K_s":False, "K_a":False, "K_d":False}
+
+    def input(self):
+        pressed = pygame.key.get_pressed()
+        '''
+        if pressed[pygame.K_w]:
+            self.direction = 'up'
+        if pressed[pygame.K_s]:
+            self.direction = 'down'
+        if pressed[pygame.K_a]:
+            self.direction = 'left'
+        if pressed[pygame.K_d]:
+            self.direction = 'right'
+        '''
+        self.keydeterm = {"K_w":False, "K_s":False, "K_a":False, "K_d":False}
+        if self.game.inputs["gesture"] == 1 or self.game.inputs["gesture"] == 2 or self.game.inputs["gesture"] == 8:
+            self.keydeterm["K_w"] = True
+        if self.game.inputs["gesture"] == 4 or self.game.inputs["gesture"] == 5 or self.game.inputs["gesture"] == 6:
+            self.keydeterm["K_s"] = True
+        if self.game.inputs["gesture"] == 2 or self.game.inputs["gesture"] == 3 or self.game.inputs["gesture"] == 4:
+            self.keydeterm["K_a"] = True
+        if self.game.inputs["gesture"] == 6 or self.game.inputs["gesture"] == 7 or self.game.inputs["gesture"] == 8:
+            self.keydeterm["K_d"] = True
+
+        if self.keydeterm["K_w"]:
+            self.direction = 'up'
+        if self.keydeterm["K_s"]:
+            self.direction = 'down'
+        if self.keydeterm["K_a"]:
+            self.direction = 'left'
+        if self.keydeterm["K_d"]:
+            self.direction = 'right'
+
+        if self.game.inputs["speech"] != " " and self.game.inputs["speech"] == "pick up":
+            self.game.object_manager.interact()
+        if self.game.inputs["speech"] == "drop it" and self.weapon:
+            self.weapon.drop()
+            if self.items:
+                self.weapon = self.items[0]
+        if pressed[pygame.K_TAB]:
+            self.game.mini_map.draw_all(self.game.screen)
+            self.game.mini_map.draw_mini_map = False
+        else:
+            self.game.mini_map.draw_mini_map = True
+        for event in pygame.event.get():
+            if event.type == pygame.MOUSEBUTTONDOWN and self.items:
+                if event.button == 4:
+                    self.weapon = self.items[self.items.index(self.weapon) - 1]
+                    self.shift_items_left()
+                    self.weapon = self.items[0]
+                elif event.button == 5:
+                    self.weapon = self.items[(self.items.index(self.weapon) + 1) % len(self.items)]
+                    self.shift_items_right()
+                    self.weapon = self.items[0]
+
+        constant_dt = self.game.dt
+        vel_up = [0, -self.speed * constant_dt]
+        vel_up = [i * self.keydeterm["K_w"] for i in vel_up]
+        vel_down = [0, self.speed * constant_dt]
+        vel_down = [i * self.keydeterm["K_s"] for i in vel_down]
+        vel_left = [-self.speed * constant_dt, 0]
+        vel_left = [i * self.keydeterm["K_a"] for i in vel_left]
+        vel_right = [self.speed * constant_dt, 0]
+        vel_right = [i * self.keydeterm["K_d"] for i in vel_right]
+        vel = zip(vel_up, vel_down, vel_left, vel_right)
+        vel_list = [sum(item) for item in vel]
+        '''
+        constant_dt = self.game.dt
+        vel_up = [0, -self.speed * constant_dt]
+        vel_up = [i * pressed[pygame.K_w] for i in vel_up]
+        vel_down = [0, self.speed * constant_dt]
+        vel_down = [i * pressed[pygame.K_s] for i in vel_down]
+        vel_left = [-self.speed * constant_dt, 0]
+        vel_left = [i * pressed[pygame.K_a] for i in vel_left]
+        vel_right = [self.speed * constant_dt, 0]
+        vel_right = [i * pressed[pygame.K_d] for i in vel_right]
+        vel = zip(vel_up, vel_down, vel_left, vel_right)
+        vel_list = [sum(item) for item in vel]
+        '''
+        x = sqrt(pow(vel_list[0], 2) + pow(vel_list[1], 2))
+
+        if 0 not in vel_list:
+            z = x / (abs(vel_list[0]) + abs(vel_list[1]))
+            vel_list_fixed = [item * z for item in vel_list]
+            self.set_velocity(vel_list_fixed)
+        else:
+            self.set_velocity(vel_list)
+
+#Localizaion ---------------
+        if(self.weapon):
+            attack_area,angle = recovery(self.game.inputs["localization"])
+            attackspeed = 1.7
+            if pygame.time.get_ticks() - self.time > attackspeed*self.attack_cooldown and self.weapon:
+                self.time = pygame.time.get_ticks()  
+                if(attack_area == 1):
+                    self.attacking = True
+                    self.weapon.weapon_swing.angle = angle
+                else:
+                    self.attacking = False
+
+                if self.weapon.name != 'staff':
+                    self.weapon.weapon_swing.swing_side *= (-1)
+#Localizaion ---------------
+
+    def shift_items_right(self):
+        self.items = [self.items[-1]] + self.items[:-1]
+
+    def shift_items_left(self):
+        self.items = self.items[1:] + [self.items[0]]
+
+    def falling_update(self):
+        if self.rect.y < self.floor_value:
+            value = 15
+            self.rect.y += value
+        else:
+            self.falling = False
+            self.game.sound_manager.play(pygame.mixer.Sound('./assets/sound/Hit.wav'))
+
+    def add_walking_particles(self):
+        if self.moving():
+            self.game.sound_manager.play_walk_sound()
+            self.game.particle_manager.add_particle(Dust(self.game, self, *self.rect.midbottom))
+
+    def update(self) -> None:
+        if self.falling:
+            self.falling_update()
+        else:
+            self.add_walking_particles()
+            if self.death_counter == 0:
+                return
+            self.entity_animation.update()
+            self.wall_collision()
+            if self.can_move:
+                self.rect.move_ip(*self.velocity)
+                self.hitbox.move_ip(*self.velocity)
+            self.detect_death()
+        if self.weapon:
+            self.weapon.update()
+        self.update_hitbox()
+
+    def fall(self, value):
+        self.rect.y = value
+        self.falling = True
+
+    def calculate_collision(self, enemy):
+        if not self.shield and not self.dead:
+            self.hp -= enemy.damage
+            self.game.sound_manager.play(self.game.sound_manager.player_hurt)
+            if not self.dead:
+                self.hurt = True
+            self.entity_animation.hurt_timer = pygame.time.get_ticks()
+        if self.shield:
+            self.shield -= 1
+            self.game.sound_manager.play(pygame.mixer.Sound('./assets/sound/Random1.wav'))
+
+    def draw(self, surface):
+        if self.death_counter == 0:
+            return
+        self.draw_shadow(surface)
+        surface.blit(self.image, self.rect)
+        if self.weapon:
+            self.weapon.draw()
---
+++ rogalik-main/src/game.py
@@ -0,0 +1,156 @@
+import pygame
+
+from .entities.enemy_manager import EnemyManager
+from .entities.player import Player
+from .menu import MainMenu
+from .mini_map import MiniMap
+from .particles import ParticleManager
+from .hud import Hud
+from .background import BackgroundEffects
+from .map.world_manager import WorldManager
+from .objects.object_manager import ObjectManager
+from .game_over import GameOver
+import time
+from .bullet import BulletManager
+from .sound_manager import SoundManager
+
+import socket
+import pickle
+import sys
+import threading
+import select
+import io
+import base64
+import zlib
+import gzip
+import struct
+
+pygame.init()
+pygame.mixer.init()
+
+world_size = (20*64, 12*64)
+
+class Game:
+    def __init__(self):
+        self.display = pygame.display.set_mode(world_size)
+        self.screen = pygame.Surface(world_size).convert()
+        self.clock = pygame.time.Clock()
+        self.enemy_manager = EnemyManager(self)
+        self.particle_manager = ParticleManager(self)
+        self.world_manager = WorldManager(self)
+        self.object_manager = ObjectManager(self)
+        self.bullet_manager = BulletManager(self)
+        self.sound_manager = SoundManager(self)
+        self.player = Player(self)
+        self.hud = Hud(self)
+        self.running = True
+        self.menu = MainMenu(self)
+        self.mini_map = MiniMap(self)
+        self.game_time = None
+        self.fps = 30
+        #self.background = BackgroundEffects()
+        self.game_over = GameOver(self)
+        pygame.mixer.init()
+        self.dt = 0
+        self.inputs = {"gesture": 0,
+                       "speech": " ",
+                       "localization": 0}
+        self.sound = pygame.mixer.Sound('./assets/sound/dungeon_theme_1.wav')
+        self.screen_position = (0, 0)
+
+    def refresh(self):
+        pygame.mixer.Sound.stop(self.sound)
+        self.__init__()
+        pygame.display.flip()
+        self.run_game()
+
+    def update_groups(self):
+        self.enemy_manager.update_enemies()
+        self.object_manager.update()
+        self.player.update()
+        self.particle_manager.update_particles()
+        self.particle_manager.update_fire_particles()
+        self.bullet_manager.update()
+        #self.background.update()
+        self.world_manager.update()
+        self.game_over.update()
+        self.mini_map.update()
+
+    def draw_groups(self):
+        #self.background.draw(self.screen)
+        self.world_manager.draw_map(self.screen)
+        if self.player:
+            self.player.draw(self.screen)
+        self.enemy_manager.draw_enemies(self.screen)
+        self.object_manager.draw()
+        self.bullet_manager.draw()
+        self.mini_map.draw(self.screen)
+        self.hud.draw()
+        self.particle_manager.draw_particles(self.world_manager.current_map.map_surface)
+        #self.particle_manager.draw_fire_particles()
+        self.game_over.draw()
+
+    def input(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+            if event.type == pygame.USEREVENT:
+                self.object_manager.up += 1
+                self.object_manager.hover = True
+
+        self.player.input()
+        pressed = pygame.key.get_pressed()
+        # if pressed[pygame.K_r]:
+        #     self.refresh()
+
+        if pressed[pygame.K_ESCAPE]:
+            if self.game_over.game_over:
+                self.refresh()
+            self.menu.running = True
+            self.menu.play_button.clicked = False
+
+    def run_game(self):
+        host = '131.179.15.231'
+        port = 12347
+        server =socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+        server.bind((host, port))
+        server.listen()
+        server.settimeout(1.0)  # Allow timeout to process KeyboardInterrupt
+        counter = 0
+        while True:
+            try:
+                client, address = server.accept()
+                if(client):
+                    break
+            except socket.timeout:
+                continue
+            
+        self.enemy_manager.add_enemies()
+        prev_time = time.time() 
+        pygame.mixer.Sound.play(self.sound, loops=-1)
+        while self.running:
+            self.clock.tick(self.fps)
+            now = time.time()
+            self.dt = now - prev_time
+            prev_time = now
+            self.menu.show()
+            self.screen.fill((0, 0, 0))
+            self.input()
+            self.update_groups()
+            self.draw_groups()
+            self.game_time = pygame.time.get_ticks()
+            data = pygame.image.tobytes(self.screen, "RGB")
+            data = zlib.compress(data)
+            size = len(data)
+            # Prefix the image data with its size
+            size_data = struct.pack("!I", size)
+            client.sendall(size_data)
+            client.sendall(data)
+            self.display.blit(self.screen, self.screen_position)
+            client.recv(1)
+            input_data = client.recv(1024)
+            self.inputs = pickle.loads(input_data)
+            print(self.inputs)
+            if self.running:
+                pygame.display.flip()
+        pygame.quit()
